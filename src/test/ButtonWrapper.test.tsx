fb4420d6f29ef2065f9e16f6216cbaaa3fc866b7
valentinesamuel <valentinesamuel2580@gmail.com>
2022-09-11 00:58:12 +0100

    feat: Tested multiple components and event handlers

---
+++ src/test/ButtonWrapper.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import ButtonWrapper from '../component/ButtonWrapper';
+
+test('handles onClick', () => {
+    const onclick = jest.fn()
+    render(<ButtonWrapper onClick={onclick} title='Add Item'/>);
+    const buttonElement = screen.getByText("Add Item");
+    fireEvent.click(buttonElement)
+    expect(onclick).toHaveBeenCalledTimes(1)
+});
