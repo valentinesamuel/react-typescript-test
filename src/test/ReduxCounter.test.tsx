0027c8564b934c13af47eaf25b92eb719443d3ad
valentinesamuel <valentinesamuel2580@gmail.com>
2022-09-17 20:42:57 +0100

    tested redux store

---
+++ src/test/ReduxCounter.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { Provider } from "react-redux";
+
+import { ReduxCounter } from "../component/ReduxCounter";
+import { createStore } from "../store";
+
+test("increment", () => {
+    render(
+        <Provider store={createStore()}>
+            <ReduxCounter />
+        </Provider>
+    );
+
+    render(<ReduxCounter />);
+
+    const counter = screen.getByRole("contentinfo")
+    expect(counter).toHaveTextContent("0")
+
+    const addButton = screen.getByText(/Increment/i);
+    fireEvent.click(addButton);
+    expect(counter).toHaveTextContent("1");
+
+})
+test("increment again", () => {
+    render(
+        <Provider store={createStore()}>
+            <ReduxCounter />
+        </Provider>
+    );
+
+    render(<ReduxCounter />);
+
+    const counter = screen.getByRole("contentinfo")
+    expect(counter).toHaveTextContent("0")
+
+    const addButton = screen.getByText(/Increment/i);
+    fireEvent.click(addButton);
+    expect(counter).toHaveTextContent("1");
+
+})
