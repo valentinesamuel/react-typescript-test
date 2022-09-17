c10c4cd046fc0a596d848b372a50cd7af348c18e
valentinesamuel <valentinesamuel2580@gmail.com>
2022-09-17 19:37:29 +0100

    ✔ Tested usestate hooks

---
+++ src/test/Counter.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { Counter } from '../component/Counter';
+
+test('handles onClick', () => {
+    render(<Counter />);
+
+    const divElement = screen.getByRole("contentinfo");
+    const buttonElement = screen.getByText('Add One')
+    fireEvent.click(buttonElement)
+
+
+    expect(divElement).toHaveTextContent("Count is 1")
+});
